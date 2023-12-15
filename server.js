66244bf2746b37893c65722d4a1c4cb96bf249cf
KiranDas96 <parvathikiran10@gmail.com>
2023-12-15 19:59:21 +0530

    Server Created

---
+++ server.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const app=express()
+require('dotenv').config()
+
+const port=process.env.PORT || 3000
+const connection=require('./db')
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.listen(port,async()=>{
+try {
+    await connection;
+    console.log("db connected successfully")
+} catch (error) {
+    console.log('error while connecting db');
+}
+console.log(`server connected in port ${port}`);
+})
