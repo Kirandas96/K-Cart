3b5aa18cce1ecb170ea3f4ba31beb7c37fbdf0cd
KiranDas96 <parvathikiran10@gmail.com>
2023-12-20 11:38:39 +0530

    user model done

--- Model/userModel.js
+++ Model/userModel.js
@@ -4,8 +4,28 @@ const {isEmail}=require('validator')
 
 
 const UseSchema=new Schema({
-    name:{},
-    emil:{},
-    password:{},
-    reg_date:{}
+    name:{
+        type:String,
+        required:true
+    },
+    emil:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        validate:[isEmail,'Please enter a valid email']
+    },
+    password:{
+        type:String,
+        required:[true,'Please enter a valid Password'],
+        minlength:[6,'Min 6 char needed']
+    },
+    reg_date:{
+        type:Date,
+        default:Date.now
+    }
 })
+
+const UserModel = mongoose.model("user", UserSchema);
+
+module.exports = UserModel;
