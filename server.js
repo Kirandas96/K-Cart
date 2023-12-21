97d810698ad2ce1b2db9b3b82e64e119815f1389
KiranDas96 <parvathikiran10@gmail.com>
2023-12-21 15:38:04 +0530

    Done

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const app=express()
 require('dotenv').config()
 
 const port=process.env.PORT 
-console.log(port)
-
-// const connection=require('./db')
 
 
-// app.use(express.json())
-// app.use(express.urlencoded({extended:true}))
+const connection=require('./db')
 
-// app.listen(port,async()=>{
-// try {
-//     await connection;
-//     console.log("db connected successfully")
-// } catch (error) {
-//     console.log('error while connecting db');
-// }
-// console.log(`server connected in port ${port}`);
-// })
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.listen(port,async()=>{
+try {
+    await connection;
+    console.log("db connected successfully")
+} catch (error) {
+    console.log('error while connecting db');
+}
+console.log(`server connected in port ${port}`);
+})
